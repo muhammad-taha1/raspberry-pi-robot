3bfdb4385c72263541d2d3831729e3809a0262c7
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2026-09-20 18:29:45 -0400

    adding speaker stuff

--- scripts/voice_test.py
+++ scripts/voice_test.py
@@ -17,7 +17,7 @@ from robotd.config import DEFAULT_CONFIG_PATH, load
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", default=str(DEFAULT_CONFIG_PATH))
-    parser.add_argument("--text", default="HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII.")
+    parser.add_argument("--text", default="hi i am raspberry pi, i am yourr personal assistant. ask me anything, i know everything")
     args = parser.parse_args()
 
     voice = PiperVoice.load(str(load(args.config).voice_model_path))
